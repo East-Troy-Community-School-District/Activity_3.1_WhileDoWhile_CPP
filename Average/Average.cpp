c5f7a18536da5cc4a4f9ffa2ccf036393d790bbb
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2023-04-16 17:42:35 -0500

    Add files via upload

---
+++ Average/Average.cpp
@@ -0,0 +1,27 @@
+/*
+Average
+Pawelski
+4/16/2023
+C++
+*/
+
+#include <iostream>
+
+int main()
+{
+    int number, sum = 0, count = 0;
+    double average;
+    std::string repeat = "Y";
+    while (repeat == "Y")
+    {
+        std::cout << "Enter a number >> ";
+        std::cin >> number;
+        // Add the entered number to sum.
+        // Increase count by 1.
+        // Ask the user if they would like to enter another number.
+        // Store the user's response in repeat.
+    }
+    // Calculate the average.
+    std::cout << "The average is " << average << ".\n";
+    return 0;
+}
